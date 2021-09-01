e3dca70b05c15321d89afa307626ce76417f09fa
majuritz <89456244+majuritz@users.noreply.github.com>
2021-09-01 12:23:24 +0200

    Genesis File

--- main/CMD_Files/PrivateNet_SetUp.cmd
+++ main/CMD_Files/PrivateNet_SetUp.cmd
@@ -4,7 +4,7 @@ cd C:/Users/rapado/AppData/Local/Ethereum/PrivateChain
 mkdir geth keystore
 
 # Netzwerk initialisieren:
-geth --http --http.port "8085" --datadir C:/Users/rapado/AppData/Local/Ethereum/PrivateChain init C:/Users/rapado/AppData/Local/Ethereum/genesis.json
+geth --http --http.port "8085" --datadir C:/Users/rapado/AppData/Local/Ethereum/PrivateChain init C:/Users/rapado/Desktop/private_geth_net/main/genesis.json
 
 # Account erstellen:
 geth --datadir=C:/Users/rapado/AppData/Local/Ethereum/PrivateChain account new
